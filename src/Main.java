04c2db0defd21d7ccbacaa62f16f676a6fa99a41
Manny Peterson <me@mannypeterson.com>
2019-04-14 12:39:40 -0500

    update

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Manny Peterson's Solution to Project Euler Problem #4
+ * https://projecteuler.net/problem=4
+ * 
+ * Enjoy! :)
+ * 
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
