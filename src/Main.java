843498b631957159d6d4a3badf56f73ed0fa03d0
Manny Peterson <me@mannypeterson.com>
2019-04-19 18:15:14 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,36 @@
  */
 
 public class Main {
-	public static void main(String[] args) {
+	private static boolean isPalindrome(int n) {
+		int workingNumber = n;
+		int reversedNumber = 0;
+		int tempNumber = 0;
+		while (workingNumber > 0) {
+			tempNumber = workingNumber % 10;
+			reversedNumber = reversedNumber * 10 + tempNumber;
+			workingNumber = workingNumber / 10;
+		}
+		if (reversedNumber == n) {
+			return true;
+		}
+		return false;
+	}
 
+	public static void main(String[] args) {
+		int biggestI = 0;
+		int biggestJ = 0;
+		int biggestProduct = 0;
+		for (int i = 100; i < 1000; i++) {
+			for (int j = 100; j < 1000; j++) {
+				if (Main.isPalindrome(i * j)) {
+					if ((i * j) > biggestProduct) {
+						biggestI = i;
+						biggestJ = j;
+						biggestProduct = i * j;
+					}
+				}
+			}
+		}
+		System.out.format("ANSWER: %d x %d = %d\n", biggestI, biggestJ, biggestProduct);
 	}
 }
